0947c1d5e38c4a60618d1459dd1605c3fccde52f
chris495b <christie.dsouza@bistasolutions.com>
2017-02-08 11:49:37 +0530

    08022017_bista

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+//global.jQuery = require("jquery")(jsdom.jsdom().defaultView),
+//global.window = jsdom.jsdom().defaultView,
+//global.document = jsdom.jsdom(),
+//global.Tether = require('tether');
 const express = require('express'),
 bodyParser= require('body-parser'),
 MongoClient = require('mongodb').MongoClient,
-stylus = require('stylus'), 
+stylus = require('stylus'),
 nib = require('nib'),
+path = require('path'),
 app = express();
+
+// console.log(bootstrap);
+
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
@@ -14,12 +22,13 @@ app.set('view engine', 'jade')
 // app.use(express.logger('dev'))
 app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }))
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(path.join(__dirname, 'node_modules/jquery')));
+app.use(express.static(path.join(__dirname, 'node_modules/tether')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap')));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
- 
-   res.render('index',{ title : 'Home' })
+   res.render('index',{ title : 'Home' });
   //res.sendFile(__dirname + '/index.html')
   // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
@@ -27,11 +36,11 @@ app.get('/', (req, res) => {
 
 app.post('/quotes', (req, res) => {
   db.collection('quotes').save(req.body, (err, result) => {
-      if (err) return console.log(err);
-      console.log('saved to database');
-      res.redirect('/');
-    });
-    
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/');
+  });
+
 });
 
 var db;
